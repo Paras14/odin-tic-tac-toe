08879728672e1f7d1a739b63b1eafd1182ba1149
Paras <parasv249@gmail.com>
2023-04-10 22:02:24 +0200

    Add player turn by turn selection and indicator

--- script.js
+++ script.js
@@ -1,39 +1,56 @@
-const gameboard = (() => {
+const gameBoard = (() => {
     let board = ['e','e','e',
                  'e','e','e',
                  'e','e','e'];
+
+    const getBoardInfo = () => board;
+
+    let victoryMark = '';
 
     const victoryArrays = [[0,1,2], [3,4,5], [6,7,8], 
                            [0,3,6], [1,4,7], [2,5,8],
                            [0,4,8], [2,4,6]];
     const markX = (index) => {
-        board[index] = 'X';
+        if(board[index] === 'e'){
+            board[index] = 'x';
+        }
     }
     const markO = (index) => {
-        board[index] = 'O';
+        if(board[index] === 'e'){
+            board[index] = 'o';
+        }
     }
     const victoryCheck = () => { //or maybe be bundled with indecisive logic
         victoryArrays.forEach((conditions) => {
-            if(board[conditions[0]] === board[conditions[1]] === board[conditions[2]]){
-                const victoryMark = board[conditions[0]];
-                return victoryMark;
+            if((board[conditions[0]] === board[conditions[1]]) && (board[conditions[1]] === board[conditions[2]]) && (board[conditions[0]] === board[conditions[2]])){
+                victoryMark = board[conditions[0]];
             }
         })
     }
     
+    const getVictoryMark = () => victoryMark;
+    
     const indecisiveCheck = () =>{
         //indecisive Logic check or maybe bundled in a single function
-    }
+        return !board.includes('e');
+    };
 
-    return {markO, markX, victoryCheck, indecisiveCheck};
+    return {markO, markX, victoryCheck, indecisiveCheck, getBoardInfo, getVictoryMark};
 })();
 
 const displayController = (() => {
     //display Controller logic and returns
-    const playerSignSelectionDisplay = (player) => {
-        const displayPrompt = document.querySelector('.game-options > p');
-        displayPrompt.innerText = `${player.getPlayerName()} should select their sign`;
-    };
+    const playerTurnIndicator = () => {
+        const playerIndicatorElements = document.querySelectorAll('.player-turn-indicator > div');
+        if(playerIndicatorElements[0].classList.contains('active-player')){
+            playerIndicatorElements[0].classList.remove('active-player');
+            playerIndicatorElements[1].classList.add('active-player');
+        }
+        else if(playerIndicatorElements[1].classList.contains('active-player')){
+            playerIndicatorElements[1].classList.remove('active-player');
+            playerIndicatorElements[0].classList.add('active-player');
+        }
+    }
 
     const boxSelector = (index) => {
         return document.querySelector(`box-${index}`);
@@ -49,45 +66,43 @@ const displayController = (() => {
         currentBox.classList.add("o-marked");
     };
 
-    return {markOonGrid, markXonGrid, playerSignSelectionDisplay};
+    return {markOonGrid, markXonGrid, playerTurnIndicator};
 })();
 
 const inputController = (() => {
-    const playerSignSelection = (player) =>{
-        const signSelectionButtons = document.querySelectorAll('.game-options button');
-        signSelectionButtons.forEach((button) => {
-            button.addEventListener("click", (e) => {
-                player.setPlayerSign(e.target.classList[0]);
-                console.log(player.getPlayerSign());
-            });
-        });
-        displayController.playerSignSelectionDisplay(player);
 
-        
-    };
-
-    const gameboard = document.querySelectorAll(".gameboard button");
-    const boxesListener = (player) => {
+    const gameboardLocal = document.querySelectorAll(".gameboard button");
+    const boxesListener = (game) => {
         console.log("inside boxListener");
-        console.log(gameboard);
-        let playerSign = player.getPlayerSign();
-        gameboard.forEach((box) => box.addEventListener("click", (e) => {
+        console.log(gameboardLocal);
+        gameboardLocal.forEach((box) => box.addEventListener("click", (e) => {
             console.log(e.target);
-            console.log(playerSign);
-            if(playerSign === 'x'){
-                console.log("inside x");
+            console.log(game.getCurrentPlayer().getPlayerName());
+            if((game.getCurrentPlayer().getPlayerSign() === 'x') && (!e.target.classList.contains('o-marked') && !e.target.classList.contains('x-marked'))){
                 e.target.classList.add('x-marked');
+                console.log(e.target.classList[0].at(-1));
+                gameBoard.markX(e.target.classList[0].at(-1));
+                console.log(gameBoard.getBoardInfo());
+                gameBoard.victoryCheck();
+                console.log("Victory Check: ",gameBoard.getVictoryMark());
+                game.switchCurrentPlayer();
+                displayController.playerTurnIndicator();
+
             }
-            else if(playerSign === 'o'){
+
+            else if((game.getCurrentPlayer().getPlayerSign() === 'o') && (!e.target.classList.contains('o-marked') && !e.target.classList.contains('x-marked'))){
                 e.target.classList.add('o-marked');
+                console.log(e.target.classList[0].at(-1));
+                gameBoard.markO(e.target.classList[0].at(-1));
+                console.log(gameBoard.getBoardInfo());
+                gameBoard.victoryCheck();
+                console.log("Victory Check: ",gameBoard.getVictoryMark());
+                game.switchCurrentPlayer();
+                displayController.playerTurnIndicator();
             }
         }));
     };
-    // const boxEntryHandler = (e, sign) => {
-        
-    // };
-
-    return {boxesListener, playerSignSelection};
+    return {boxesListener};
 })();
 
 const player = (playerName) => {
@@ -101,27 +116,47 @@ const player = (playerName) => {
     return {getPlayerName, getPlayerSign, setPlayerSign};
 };
 
-const game = (gameboard, displayController) => {
+const game = () => {
 
     
     //playerOne
-    const playerOne = player('Player One');
+    const playerOne = player('first-player');
     //playerTwo
-    const playerTwo = player('Player Two');
+    const playerTwo = player('second-player');
 
     //game flow logic
-    const firstTurnSelector = () => {
-        //first turn selector and display
+    const playerSignSetter = () => {
+        playerOne.setPlayerSign('x');
+        playerTwo.setPlayerSign('o');
+    };
+
+    let currentPlayer = playerOne;
+
+    const setCurrentPlayer = (player) => {
+        currentPlayer = player;
     }
+
+    const getCurrentPlayer = () => currentPlayer;
+
+    const switchCurrentPlayer = () => {
+        if(currentPlayer === playerOne){
+            currentPlayer = playerTwo;
+        }
+        else if(currentPlayer === playerTwo){
+            currentPlayer = playerOne;
+        }
+    };
     //each round logic and victory check and match progress display
+
+    return {playerSignSetter, switchCurrentPlayer, setCurrentPlayer, getCurrentPlayer};
 };
 
 function gameFlow(){
     console.log("inside game flow");
-    const playerOne = player('Paras');
-    playerOne.setPlayerSign('x');
-    console.log(playerOne.getPlayerSign());
-    inputController.boxesListener(playerOne);
+    const newGame = game();
+    newGame.playerSignSetter();
+    inputController.boxesListener(newGame);
+    
 }
 
 gameFlow();
